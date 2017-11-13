7c04cf091a334ddd08e3071d3abb8a75355b0c29
Shannon Tully <scatman.st@gmail.com>
2017-11-13 14:49:05 -0800

    all locations populate

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var firstAndPike = {
   maxCust: 65,
   avgCookieBought: 6.3,
   cookies: [],
+  dayTotal: null,
   getRandom: function() {
     var min = Math.ceil(this.minCust);
     var max = Math.floor(this.maxCust);
@@ -12,7 +13,88 @@ var firstAndPike = {
   },
   calulate: function() {
     for(var i = 0; i < 14; i++) {
-      this.cookies[i] = this.getRandom() * this.avgCookieBought;
+      this.cookies[i] = Math.round(this.getRandom() * this.avgCookieBought);
+      this.dayTotal += this.cookies[i];
+    }
+    return this.cookies;
+  },
+};
+
+var seatac = {
+  minCust: 3,
+  maxCust: 24,
+  avgCookieBought: 1.2,
+  cookies: [],
+  dayTotal: null,
+  getRandom: function() {
+    var min = Math.ceil(this.minCust);
+    var max = Math.floor(this.maxCust);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  },
+  calulate: function() {
+    for(var i = 0; i < 14; i++) {
+      this.cookies[i] = Math.round(this.getRandom() * this.avgCookieBought);
+      this.dayTotal += this.cookies[i];
+    }
+    return this.cookies;
+  },
+};
+
+var seattleCenter = {
+  minCust: 11,
+  maxCust: 38,
+  avgCookieBought: 3.7,
+  cookies: [],
+  dayTotal: null,
+  getRandom: function() {
+    var min = Math.ceil(this.minCust);
+    var max = Math.floor(this.maxCust);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  },
+  calulate: function() {
+    for(var i = 0; i < 14; i++) {
+      this.cookies[i] = Math.round(this.getRandom() * this.avgCookieBought);
+      this.dayTotal += this.cookies[i];
+    }
+    return this.cookies;
+  },
+};
+
+var capHill = {
+  minCust: 20,
+  maxCust: 38,
+  avgCookieBought: 2.3,
+  cookies: [],
+  dayTotal: null,
+  getRandom: function() {
+    var min = Math.ceil(this.minCust);
+    var max = Math.floor(this.maxCust);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  },
+  calulate: function() {
+    for(var i = 0; i < 14; i++) {
+      this.cookies[i] = Math.round(this.getRandom() * this.avgCookieBought);
+      this.dayTotal += this.cookies[i];
+    }
+    return this.cookies;
+  },
+};
+
+var Alki = {
+  minCust: 2,
+  maxCust: 16,
+  avgCookieBought: 4.6,
+  cookies: [],
+  dayTotal: null,
+  getRandom: function() {
+    var min = Math.ceil(this.minCust);
+    var max = Math.floor(this.maxCust);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  },
+  calulate: function() {
+    for(var i = 0; i < 14; i++) {
+      this.cookies[i] = Math.round(this.getRandom() * this.avgCookieBought);
+      this.dayTotal += this.cookies[i];
     }
     return this.cookies;
   },
